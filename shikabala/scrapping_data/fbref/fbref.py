848e3da46184e084a30561a569c12784fe7d3c0d
karim <amer@karims-MBP.lan>
2022-04-24 22:43:25 -0700

    WIP commits

---
+++ shikabala/scrapping_data/fbref/fbref.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def readfromhtml(filepath):
+    """
+    x = readfromhtml(
+        "https://fbref.com/en/comps/Big5/shooting/players/Big-5-European-Leagues-Stats"
+    )
+    """
+
+    df = pd.read_html(filepath)[0]
+    column_lst = list(df.columns)
+    for index in range(len(column_lst)):
+        column_lst[index] = column_lst[index][1]
+
+    df.columns = column_lst
+    df.drop(df[df["Player"] == "Player"].index, inplace=True)
+    df = df.fillna("0")
+    df.set_index("Rk", drop=True, inplace=True)
+    try:
+        df["Comp"] = df["Comp"].apply(lambda x: " ".join(x.split()[1:]))
+        df["Nation"] = df["Nation"].astype(str)
+        df["Nation"] = df["Nation"].apply(lambda x: x.split()[-1])
+    except ValueError:
+        print("Error in uploading file:" + filepath)
+    finally:
+        df = df.apply(pd.to_numeric, errors="ignore")
+        return df
--- shikabala/scrapping_data/understat/understat.py
+++ shikabala/scrapping_data/understat/understat.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 BASE_URL = "https://understat.com/match/"
 BASE_LEAGUE_URL = "https://understat.com/league/"
+BASE_LEAGUE_PLAYER = "https://understat.com/player/"
 
 
 def scrape(id: str, type: str, league=None) -> str:
@@ -26,6 +27,8 @@ def scrape(id: str, type: str, league=None) -> str:
                     URL = BASE_LEAGUE_URL + "Ligue_1/" + str(id)
                 case "RFPL":
                     URL = BASE_LEAGUE_URL + "RFPL/" + str(id)
+        case "player":
+            URL = BASE_LEAGUE_PLAYER + id
 
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, "lxml")
@@ -52,7 +55,7 @@ def create_df(id: int) -> pd.DataFrame:
     return df
 
 
-def generate_players_dict(year: str, league: str) -> Dict:
+def generate_players_season_dict(year: str, league: str) -> Dict:
 
     soup_scripts = scrape(year, "season", league)
 
@@ -64,3 +67,43 @@ def generate_players_dict(year: str, league: str) -> Dict:
     json_string = json_string.encode("utf8").decode("unicode_escape")
     players_dict = json.loads(json_string)
     return players_dict
+
+
+def generate_player(id):
+    soup_scripts = scrape(id, "player")
+
+    strings = soup_scripts[3].string
+    start_index = strings.index("('") + 2
+    end_index = strings.index("')")
+    json_data = strings[start_index:end_index]
+    json_data = json_data.encode("utf8").decode("unicode_escape")
+    data = json.loads(json_data)
+    return data
+
+
+def create_player_df(data):
+    x, y, xg, result, season = [], [], [], [], []
+
+    for i, _ in enumerate(data):
+        for key in data[i]:
+            if key == "X":
+                x.append(data[i][key])
+            if key == "Y":
+                y.append(data[i][key])
+            if key == "xG":
+                xg.append(data[i][key])
+            if key == "result":
+                result.append(data[i][key])
+            if key == "season":
+                season.append(data[i][key])
+
+    columns = ["X", "Y", "xG", "Result", "Season"]
+    df_understat = pd.DataFrame([x, y, xg, result, season], index=columns)
+    df_understat = df_understat.T
+    df_understat = df_understat.apply(pd.to_numeric, errors="ignore")
+
+    return df_understat
+
+
+data = generate_player("1250")
+create_player_df(data)
